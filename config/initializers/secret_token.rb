44d01f2c85db34a34788f1d57ea7ef0a72d8ef70
Ismail Akram <rubyonrails3@gmail.com>
2011-06-17 10:25:09 +0500

    inital commit with blog scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '85bc20d97fadab2ce0bfd4f96761a5c35dccc7f13c80fac96898e149f6678d42af2c064611615d1c177ae629ed223ff5ca5f953540d9f20616e6a98f741f8d3e'
